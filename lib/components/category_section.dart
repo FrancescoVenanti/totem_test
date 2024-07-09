1d55121c280315e19f5f7b24b941a156eeb8771f
FrancescoVenanti <francesco.venanti@gmail.com>
2024-07-09 09:42:24 +0200

    logic for category

--- lib/components/category_section.dart
+++ lib/components/category_section.dart
@@ -6,17 +6,21 @@ class CategorySection extends StatelessWidget {
   CategorySection({
     super.key,
   });
-  final List<Widget> categorie = [];
+  /*  final List<Widget> categorie = []; */
   @override
   Widget build(BuildContext context) {
-    for (var i = 0; i < Utils.categories.length; i++) {
+    /* for (var i = 0; i < Utils.categories.length; i++) {
       categorie.add(SingleCategory(
           text: Utils.categories[i].name, image: Utils.categories[i].image));
-    }
+    } */
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Column(
-        children: categorie,
+        //children: categorie,
+        children: [
+          for (var e in Utils.categories)
+            SingleCategory(text: e.name, image: e.image)
+        ],
       ),
     );
   }
